1dcc80a238522038df46c1e3cd1bdbd7a2b7efd5
sanika-bhor <bhorsanika0239@gmail.com>
2024-12-12 18:56:54 +0530

    basic of .net like data types,if-else, while

---
+++ basic/basic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("welcome tho the world of .NET");
+
+            //count variable with intial value 10
+            int count = 10;
+            Console.WriteLine(count);
+
+            //read and display data
+            string name;
+            Console.WriteLine("enter name");
+            name = Console.ReadLine();
+            Console.WriteLine("your Name {0}: ", name); //way 1 to display data
+            Console.WriteLine("your name :" + name);  //another way
+
+            //increment count till 50 ad display result
+            if(count <=10)
+            {
+                while(count <= 50)
+                {
+                    Console.WriteLine("count {0}", count);
+                    count++;
+                }
+            }
+        }
+    }
+}

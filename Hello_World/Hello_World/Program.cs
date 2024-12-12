16c91abeaf35dbe4b601f53325878a906d6f367a
sanika-bhor <bhorsanika0239@gmail.com>
2024-12-12 18:26:40 +0530

    first dotnet program- 'hello world'

---
+++ Hello_World/Hello_World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !!");
+        }
+    }
+}

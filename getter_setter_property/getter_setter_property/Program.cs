bfd9c9e2e95a61438f5bdad88ce89c1565e4301f
sanika-bhor <bhorsanika0239@gmail.com>
2024-12-13 11:30:52 +0530

    get set property

---
+++ getter_setter_property/getter_setter_property/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getter_setter_property
+{
+    internal class Program
+    {
+
+        string Uname;   //field
+
+      
+        //simple getter setter
+         string getter()
+        {
+            return Uname;
+        }
+
+        void setter(string name)
+        {
+            Uname = name;
+        }
+
+
+        //get set property
+        public string data   //property
+        {
+            get { return Uname; }
+            set { Uname = value; } // here value is a predefined keyword
+        }
+        static void Main(string[] args)
+        {
+            string name,surname;
+            Program program = new Program();
+
+            //using simple getter setter
+            Console.WriteLine("enter name: ");
+            name= Console.ReadLine();
+            program.setter(name);
+            Console.WriteLine("name: {0}",program.getter());
+
+
+            //using set get property
+            Console.WriteLine("enter surname: ");
+            surname = Console.ReadLine();
+            program.data = surname;
+            Console.WriteLine("your surname: {0}", program.data);
+            
+
+       
+    }
+    }
+}

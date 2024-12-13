cbe4a210fefc708a6325b600be791f4c6200df8b
sanika-bhor <bhorsanika0239@gmail.com>
2024-12-13 20:01:59 +0530

    tester app

---
+++ TesterApp/TesterApp/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    public class Account
+    {
+        private float balance;
+
+        public Account(float Amount) {
+            this.balance = Amount;
+        }
+
+        public float Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+
+        public void withdraw(float amount)
+        {
+            balance = balance - amount;
+        }
+
+        public void deposite(float amount)
+        {
+            balance = balance + amount;
+        }
+    }
+}
---
+++ TesterApp/TesterApp/Product.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog
+{
+    public class Product
+    {
+        private int id;
+        private string tittle;
+        private string discription;
+        private int quntity;
+        private int unitPrice;
+
+
+        public Product()
+        {
+            this.id = 1;
+            this.tittle = "rose";
+            this.discription = "valentine flower";
+            this.quntity = 20;
+            this.unitPrice = 5;
+        }
+
+        public Product(int id, string tittle, string discription, int quntity, int unitprice)
+        {
+            this.id = id;
+            this.tittle = tittle;
+            this.discription = discription;
+            this.quntity = quntity;
+            this.unitPrice = unitPrice;
+            
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Tittle
+        {
+            get { return tittle; }
+            set{tittle=value;}
+        }
+
+        public string Discription
+        {
+            get { return discription; }
+            set { discription = value; }
+        }
+
+        public int Quantity
+        {
+            get { return Quantity; }
+            set { Quantity = value; }
+        }
+        public int UnitPrice
+        {
+            get { return UnitPrice; }
+            set { UnitPrice = value; }
+        }
+    }
+}

50015dd7d86a4fa166f14935ff2611af4d89bfce
sanika-bhor <bhorsanika0239@gmail.com>
2024-12-13 20:02:37 +0530

    tester app

--- TesterApp/TesterApp/Customer.cs
+++ TesterApp/TesterApp/Customer.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//namespace - logical collection of .net type like class
+namespace CRM
+{
+    public class Customer
+    {
+        public string LoginId { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int ContactNo { get; set; }
+        public string Location { get; set; }
+    }
+}

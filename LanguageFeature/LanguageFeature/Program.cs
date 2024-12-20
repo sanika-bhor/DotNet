55e231adf1c5eb5da8bd951bc23b27ccb2c72d74
sanika-bhor <bhorsanika0239@gmail.com>
2024-12-20 23:08:38 +0530

    c# language features

--- LanguageFeature/LanguageFeature/Program.cs
+++ LanguageFeature/LanguageFeature/Program.cs
@@ -16,9 +16,16 @@ namespace LanguageFeature
         void stop();
     }
 
+    interface IScannable
+    {
+        void scanning();
+    }
+
     //prototype is implemented by concreate class
     //concreate class: A class whose object has been created called as concreate class
-    class Printer:IPrintable
+
+    //multiple interface inheritance but multiple class inheritance in dotnet is not allowed
+    class Printer :IPrintable,IScannable 
     {
         void IPrintable.print()
         {
@@ -33,9 +40,14 @@ namespace LanguageFeature
         {
             Console.WriteLine("printer is stop");
         }
+
+        void IScannable.scanning()
+        {
+            Console.WriteLine("Scanning printing data");
+        }
     }
 
-    class ThreeDPrinter : IPrintable 
+    class ThreeDPrinter : IPrintable,IScannable
     {
         void IPrintable.print()
         {
@@ -49,6 +61,10 @@ namespace LanguageFeature
         void IPrintable.stop()
         {
             Console.WriteLine("3D printer is stop");
+        }
+        void IScannable.scanning()
+        {
+            Console.WriteLine("Scanning 3D printing model");
         }
     }
      class CSharpLanguageFeature
@@ -68,17 +84,38 @@ namespace LanguageFeature
             //heap is managed by garbage collector
 
             CSharpLanguageFeature languageFeature=new CSharpLanguageFeature();
-            IPrintable outpuDevices = null;
 
+
+            Console.WriteLine("Interface demo");
+            Console.WriteLine();
+            IPrintable outpuDevices = null;
+            IScannable outpuFeatures = null;
+
+            Console.WriteLine("*** 2D Printer ***");
             Printer printer=new Printer();
             outpuDevices = new Printer();
+            outpuFeatures = new Printer();
+            
+            //late binding
+            outpuFeatures.scanning();
+            outpuDevices.start();
             outpuDevices.print();
+            outpuDevices.stop();
+            Console.WriteLine();
 
-            ThreeDPrinter threeDPrinter= new ThreeDPrinter();
+            Console.WriteLine("*** 3D Printer ***");
+            ThreeDPrinter threeDPrinter = new ThreeDPrinter();
             outpuDevices = new ThreeDPrinter();
+            outpuFeatures = new ThreeDPrinter();
+
+            //late binding
+            outpuFeatures.scanning();
+            outpuDevices.start();
             outpuDevices.print();
+            outpuDevices.stop();
 
 
+          
 
             Console.WriteLine("Hello World");
 

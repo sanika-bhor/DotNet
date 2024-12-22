e0391d6ce7f4ed89144fb3934493be25baa8e423
sanika-bhor <bhorsanika0239@gmail.com>
2024-12-22 13:11:54 +0530

    boxing and unboxing

---
+++ BoxingUnboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BoxingUnboxing
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //value type
+            int count = 8;
+            Console.WriteLine("value type variable - count: {0}",count);
+
+
+            //reference type
+            //object is a root classs for all the classes
+            Object obj=new object();
+
+            //boxining is a technique of converting the value type variable to refernce type
+            obj = count;
+            Console.WriteLine("reference type object - obj: {0}", obj);
+
+            //unboxing is a technique of converting the refernce type object to value type by the explicit type casting
+            int unboxingCount = (int)obj;
+            Console.WriteLine("value type variable - unboxingCount: {0}", unboxingCount);
+        }
+    }
+}

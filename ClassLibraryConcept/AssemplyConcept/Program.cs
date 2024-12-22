f5837c676ed5c8e34412de7482c13ac9cdc940ad
sanika-bhor <bhorsanika0239@gmail.com>
2024-12-22 21:22:55 +0530

    classlibrary concept folder

---
+++ ClassLibraryConcept/AssemplyConcept/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemplyConcept
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 20;
+            int num2 = 28;
+
+
+        }
+    }
+}
---
+++ ClassLibraryConcept/MathEngine/MathEngine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathEngine
+{
+    public static class MathEngine
+    {
+        public static int addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int subraction (int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}

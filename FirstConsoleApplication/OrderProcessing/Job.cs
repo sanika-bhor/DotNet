a7f471b0d1f750a61a036791d826e73a046be0cd
sanika-bhor <bhorsanika0239@gmail.com>
2024-12-23 00:02:29 +0530

    order processing: job, order

---
+++ FirstConsoleApplication/OrderProcessing/Job.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessing
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public StringInfo Title { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ FirstConsoleApplication/OrderProcessing/Order.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OrderProcessing
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime orderDate { get; set; }
+        public Order(){ }
+    }
+}

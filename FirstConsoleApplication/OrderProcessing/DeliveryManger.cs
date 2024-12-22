5111ce2e8e464c91172857047edcaca5f81dd705
sanika-bhor <bhorsanika0239@gmail.com>
2024-12-23 00:30:08 +0530

    order processing

---
+++ FirstConsoleApplication/OrderProcessing/DeliveryManger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessing
+{
+    public class DeliveryManger:Manager
+    {
+        List<Order> orderData = new List<Order>();
+        
+        public void insert(Order order)
+        {
+            orderData.Add(order);
+        }
+       
+        public void update(Order order)
+        {
+            orderData.Remove(order);
+            orderData.Add(order);
+
+            Console.WriteLine("ordere data updates");
+        }
+        public void delete(Order order)
+        {
+            orderData.Remove(order);
+        }
+
+        public List<Order> getAll()
+        {
+            return orderData;
+        }
+
+        public Order getOrderById(int id)
+        {
+            return new Order { OrderId = id };
+        }
+        
+        public Order getOrderByVendor(string vendor)
+        {
+            return new WorkOrder { Vendor=vendor};
+        }
+        */
+    }
+}
--- FirstConsoleApplication/OrderProcessing/Job.cs
+++ FirstConsoleApplication/OrderProcessing/Job.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OrderProcessing
+﻿namespace OrderProcessing
 {
     public class Job
     {
         public int Id { get; set; }
-        public StringInfo Title { get; set; }
+        public string Title { get; set; }
         public string Description { get; set; }
         public int Duration { get; set; }
         public bool Status { get; set; }

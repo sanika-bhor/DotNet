b2ca41214c870c75a8aa671927f0d39496a08f81
sanika-bhor <bhorsanika0239@gmail.com>
2024-12-22 13:28:33 +0530

    readonly and const

---
+++ ConstReadonly/ConstReadonly/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstReadonly
+{
+    public class MathEngine
+    {
+        public readonly double PI; //at the time of declaration it can or cannot be initialized
+        public const int count= 32; //at the time of declaration it must have to initialized
+
+        public MathEngine()
+        {
+            PI = 3.14; // initialized only once
+            // count = 35: not allow
+        }
+
+        public void display()
+        {
+            // count = 35: not allow
+            // pi = 35: not allow
+
+            Console.WriteLine("count: {0}",count);
+            Console.WriteLine("PI: {0}",PI);
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MathEngine mathEngine = new MathEngine();
+            mathEngine.display();
+        }
+    }
+}

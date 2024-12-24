915b497414ea573a86c94624d19896327eace8d8
sanika-bhor <bhorsanika0239@gmail.com>
2024-12-24 18:48:55 +0530

    PurchaseOrderService

---
+++ FirstConsoleApplication/OrderProcessing/IOrderService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessing
+{
+     public interface IOrderService
+    {
+        public void Cancel(Order order);
+        public void create(Order order);
+        public void getOrder(int id);
+        public void update(Order order);
+        public List<Order> getAllOrders();
+        public bool process(Order order);
+    }
+}
---
+++ FirstConsoleApplication/OrderProcessing/PurchaseOrderService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessing
+{
+    public class PurchaseOrderService : IOrderService
+    {
+        PurchaseManager purchaseManager = new PurchaseManager();
+        public void Cancel(Order order)
+        {
+            purchaseManager.delete(order);
+        }
+        public void create(Order order)
+        {
+            purchaseManager.insert(order);
+        }
+
+        public void getOrder(int id)
+        {
+            purchaseManager.getOrderById(id);
+        }
+
+        public void update(Order order)
+        {
+            purchaseManager.update(order);
+        }
+        public List<Order> getAllOrders()
+        {
+            List<Order>order= purchaseManager.getAll();
+            return order;
+        }
+
+        public bool process(Order order)
+        {
+            bool status = true;
+            return status; ;
+        }
+    }
+}

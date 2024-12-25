89ac085a72c183ce966569e45cbb51832b24ff49
sanika-bhor <bhorsanika0239@gmail.com>
2024-12-25 14:51:39 +0530

    account class with deposite and withdraw function

---
+++ EventHandling/EventHandling/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHandling
+{
+    public class Account
+    {
+        public int Balance { get; set; }
+
+        public Account(int balance)
+        {
+            this.Balance = balance;
+        }
+
+        public void withdraw(int amount)
+        {
+            Balance -= amount;
+        }
+
+        public void deposite(int amount)
+        {
+            Balance +=amount;
+        }
+    }
+}
---
+++ EventHandling/EventHandling/EventHandlingTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHandling
+{
+    public class EventHandlingTest
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(500);
+
+            Console.WriteLine("Initial balance:{0} ", acc.Balance);
+
+            acc.deposite(562);
+            Console.WriteLine("after deposite: {0}", acc.Balance);
+
+            acc.withdraw(200);
+            Console.WriteLine("after withdraw: {0}", acc.Balance);
+
+        }
+    }
+}

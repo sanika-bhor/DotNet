5d12bb30250d57513888732797b0ec96dec3dec8
sanika-bhor <bhorsanika0239@gmail.com>
2024-12-27 13:46:05 +0530

    Language integrated query

--- WindowFormApplication/BLL/BusinessManager.cs
+++ WindowFormApplication/BLL/BusinessManager.cs
@@ -15,7 +15,7 @@ namespace BLL
 
             Product Rose = new Product(101, "rose", "valentine flower", 4523, 20);
             Product Aster = new Product(101, "aster", "festival flower", 563210, 5);
-            Product Gerbera = new Product(101, "gerberaa", "merrage flower", 7423, 30);
+            Product Gerbera = new Product(101, "gerberaa", "merrage flower", 0, 30);
             Product Lotus = new Product(101, "rose", "unique flower", 562, 50);
 
             allProducts.Add(Rose);
@@ -24,6 +24,15 @@ namespace BLL
             allProducts.Add(Lotus);
 
             return allProducts;
+        }
+
+        public static IEnumerable<Product> getSoldProducts()
+        {
+            List<Product> allProduct = BusinessManager.getAllProducts();
+            IEnumerable<Product> soldProducts= (IEnumerable<Product>) from product in allProduct
+                                        where product.Quantity==0
+                                        select product;
+            return soldProducts;
         }
     }
 }
--- WindowFormApplication/FirstConsoleApplication/LINQTest.cs
+++ WindowFormApplication/FirstConsoleApplication/LINQTest.cs
@@ -13,7 +13,15 @@ namespace FirstConsoleApplication
         public static void Main(string[] args)
         {
             List<Product> allProduct = BusinessManager.getAllProducts();
+            Console.WriteLine("All Products: ");
             foreach(Product p in allProduct)
+            {
+                Console.WriteLine(p.Tittle);
+            }
+
+            IEnumerable<Product> soldProduct = BusinessManager.getSoldProducts();
+            Console.WriteLine("\nSold Products: ");
+            foreach (Product p in soldProduct)
             {
                 Console.WriteLine(p.Tittle);
             }

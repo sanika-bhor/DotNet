e03a65554be99c99f9b251acc974a3d3d6905960
sanika-bhor <bhorsanika0239@gmail.com>
2024-12-27 13:17:47 +0530

    Language integrated query

---
+++ WindowFormApplication/BLL/BusinessManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalog;
+
+namespace BLL
+{
+    public static class BusinessManager
+    {
+        public static List<Product> getAllProducts()
+        {
+            List<Product> allProducts = new List<Product>();
+
+            Product Rose = new Product(101, "rose", "valentine flower", 4523, 20);
+            Product Aster = new Product(101, "aster", "festival flower", 563210, 5);
+            Product Gerbera = new Product(101, "gerberaa", "merrage flower", 7423, 30);
+            Product Lotus = new Product(101, "rose", "unique flower", 562, 50);
+
+            allProducts.Add(Rose);
+            allProducts.Add(Aster);
+            allProducts.Add(Gerbera);
+            allProducts.Add(Lotus);
+
+            return allProducts;
+        }
+    }
+}
---
+++ WindowFormApplication/FirstConsoleApplication/LINQTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalog;
+using BLL;
+
+namespace FirstConsoleApplication
+{
+    public class LINQTest
+    {
+        public static void Main(string[] args)
+        {
+            List<Product> allProduct = BusinessManager.getAllProducts();
+            foreach(Product p in allProduct)
+            {
+                Console.WriteLine(p.Tittle);
+            }
+        }
+    }
+}

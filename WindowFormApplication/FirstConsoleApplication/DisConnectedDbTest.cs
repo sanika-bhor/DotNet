a8d6da375edcfc0190cf028bacdf1e40ab142824
sanika-bhor <bhorsanika0239@gmail.com>
2024-12-31 15:02:30 +0530

    changes added

--- WindowFormApplication/FirstConsoleApplication/DisConnectedDbTest.cs
+++ WindowFormApplication/FirstConsoleApplication/DisConnectedDbTest.cs
@@ -12,127 +12,145 @@ namespace FirstConsoleApplication
     {
         public static void Main(string[] args)
         {
-
-           Console.WriteLine("product data");
-            List<Product> all = BusinessManagerForDisConnected.getAllDbProducts();
-            foreach (Product p in all)
+            int ch;
+            do
             {
-                Console.WriteLine(p.Id + "  " + p.Tittle);
-            }
+                Console.WriteLine("Enter your choice: ");
+                Console.WriteLine("1.Get All Products");
+                Console.WriteLine("2.Get All Sold Products");
+                Console.WriteLine("3.Get Products by id");
+                Console.WriteLine("4.Insert");
+                Console.WriteLine("5.Update");
+                Console.WriteLine("6.Delete");
+                ch = int.Parse(Console.ReadLine());
 
+                switch (ch)
+                {
+                    case 1:
+                        Console.WriteLine("product data");
+                        List<Product> all = BusinessManagerForDisConnected.getAllDbProducts();
+                        foreach (Product p in all)
+                        {
+                            Console.WriteLine(p.Id + "  " + p.Tittle);
+                        }
+                        break;
 
+                    case 2:
+                        Console.WriteLine("\nsold products: ");
+                        List<Product> soldProducts = BusinessManagerForConnected.getSoldProductsFromDb();
+                        foreach (Product p in soldProducts)
+                        {
+                            Console.WriteLine(p.Id + "  " + p.Tittle);
+                        }
+                        break;
 
-           /* Console.WriteLine("\nsold products: ");
-            List<Product> soldProducts = BusinessManagerForConnected.getSoldProductsFromDb();
-            foreach (Product p in soldProducts)
-            {
-                Console.WriteLine(p.Id + "  " + p.Tittle);
-            }*/
-
-
-
-
-
-            /* Console.WriteLine("\ninsert new products: ");
-              Console.WriteLine("Enter product id");
-              int id = int.Parse(Console.ReadLine());
-
-              Console.WriteLine("Enter product Title");
-              string title = Console.ReadLine();
-
-              Console.WriteLine("Enter product description");
-              string description = Console.ReadLine();
-
-              Console.WriteLine("Enter product Quantity");
-              int quantity = int.Parse(Console.ReadLine());
-
-              Console.WriteLine("Enter product unit Price");
-              int unitPrice = int.Parse(Console.ReadLine());
-
-              Product product = new Product
-              {
-                  Id = id,
-                  Tittle = title,
-                  Discription = description,
-                  UnitPrice = unitPrice,
-                  Quantity = quantity
-              };
-
-              bool insertStatus = BusinessManagerForDisConnected.insertProductInDb(product);
-              if(insertStatus)
-              {
-                  Console.WriteLine("Product inserted successfully");
-              }
-              else
-              {
-                  Console.WriteLine("Product not inserted successfully");
-              }
-            */
+                    case 3:
+                        Console.WriteLine("\nget products by id: ");
+                         Console.WriteLine("Enter product id");
+                         int id = int.Parse(Console.ReadLine());
             
+                         Product product = BusinessManagerForDisConnected.getProductById(id);
+                         Console.WriteLine(product.Id+"  "+product.Tittle+"  "+product.Discription+"  "+product.UnitPrice+"  "+product.Quantity);
             
-            
-              /* Console.WriteLine("\ndelete existing products: ");
-              Console.WriteLine("Enter product id");
-              int Productid = int.Parse(Console.ReadLine());
+                        break;
 
-              bool deleteResult = BusinessManagerForDisConnected.deleteFromProduct(Productid);
-              if(deleteResult)
-              {
-                  Console.WriteLine("Product deleted successfully");
-              }
-              else
-              {
-                  Console.WriteLine("Product not deleted ");
-              }
-              */
-            
-            
-            
-            
-            /*Console.WriteLine("\nget products by id: ");
-             Console.WriteLine("Enter product id");
-             int id = int.Parse(Console.ReadLine());
-            
-             Product product = BusinessManagerForDisConnected.getProductById(id);
-             Console.WriteLine(product.Id+"  "+product.Tittle+"  "+product.Discription+"  "+product.UnitPrice+"  "+product.Quantity);
-            */
+                    case 4:
+                         Console.WriteLine("\ninsert new products: ");
+                         Console.WriteLine("Enter product id");
+                         int Newid = int.Parse(Console.ReadLine());
 
+                         Console.WriteLine("Enter product Title");
+                         string Newtitle = Console.ReadLine();
 
+                         Console.WriteLine("Enter product description");
+                         string Newdescription = Console.ReadLine();
 
-          /* Console.WriteLine("\nUpdate existing products by its id: ");
-            Console.WriteLine("Enter product id");
-            int id = int.Parse(Console.ReadLine());
+                         Console.WriteLine("Enter product Quantity");
+                         int Newquantity = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter product Title");
-            string title = Console.ReadLine();
+                         Console.WriteLine("Enter product unit Price");
+                         int NewunitPrice = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter product description");
-            string description = Console.ReadLine();
+                         Product Newproduct = new Product
+                         {
+                             Id = Newid,
+                             Tittle = Newtitle,
+                             Discription = Newdescription,
+                             UnitPrice = NewunitPrice,
+                             Quantity = Newquantity
+                         };
 
-            Console.WriteLine("Enter product Quantity");
-            int quantity = int.Parse(Console.ReadLine());
+                         bool insertStatus = BusinessManagerForDisConnected.insertProductInDb(Newproduct);
+                         if(insertStatus)
+                         {
+                             Console.WriteLine("Product inserted successfully");
+                         }
+                         else
+                         {
+                             Console.WriteLine("Product not inserted successfully");
+                         }
+                        break;
 
-            Console.WriteLine("Enter product unit Price");
-            int unitPrice = int.Parse(Console.ReadLine());
+                    case 5:
+                        Console.WriteLine("\nUpdate existing products by its id: ");
+                          Console.WriteLine("Enter product id");
+                          int Updatedid = int.Parse(Console.ReadLine());
 
-            Product product = new Product
-            {
-                Id = id,
-                Tittle = title,
-                Discription = description,
-                UnitPrice = unitPrice,
-                Quantity = quantity
-            };
+                          Console.WriteLine("Enter product Title");
+                          string Updatedtitle = Console.ReadLine();
 
-            bool insertStatus = BusinessManagerForDisConnected.UpdateProductById(product);
-            if (insertStatus)
-            {
-                Console.WriteLine("Product update successfully");
-            }
-            else
-            {
-                Console.WriteLine("Product not update");
-            }*/
+                          Console.WriteLine("Enter product description");
+                          string Updateddescription = Console.ReadLine();
+
+                          Console.WriteLine("Enter product Quantity");
+                          int Updatedquantity = int.Parse(Console.ReadLine());
+
+                          Console.WriteLine("Enter product unit Price");
+                          int UpdatedunitPrice = int.Parse(Console.ReadLine());
+
+                          Product Updatedproduct = new Product
+                          {
+                              Id = Updatedid,
+                              Tittle = Updatedtitle,
+                              Discription = Updateddescription,
+                              UnitPrice = UpdatedunitPrice,
+                              Quantity = Updatedquantity
+                          };
+
+                          bool UpdatedStatus = BusinessManagerForDisConnected.UpdateProductById(Updatedproduct);
+                          if (UpdatedStatus)
+                          {
+                              Console.WriteLine("Product update successfully");
+                          }
+                          else
+                          {
+                              Console.WriteLine("Product not update");
+                          }
+                        break;
+
+                    case 6:
+                        Console.WriteLine("\ndelete existing products: ");
+                        Console.WriteLine("Enter product id");
+                        int Productid = int.Parse(Console.ReadLine());
+
+                        bool deleteResult = BusinessManagerForDisConnected.deleteFromProduct(Productid);
+                        if(deleteResult)
+                        {
+                            Console.WriteLine("Product deleted successfully");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Product not deleted ");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("THANK YOU!!");
+                        break;
+
+                }
+
+            } while (ch != 'Y' || ch != 'y');        
         }
     }
 }

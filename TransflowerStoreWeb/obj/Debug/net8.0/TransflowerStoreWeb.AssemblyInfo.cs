f597c6b6d48d641c62ab549308b7b6d44cd3af77
sanika-bhor <bhorsanika0239@gmail.com>
2025-01-03 14:40:12 +0530

    asp  core application

--- TransflowerStoreWeb/obj/Debug/net8.0/TransflowerStoreWeb.AssemblyInfo.cs
+++ TransflowerStoreWeb/obj/Debug/net8.0/TransflowerStoreWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TransflowerStoreWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efbf31e9e37db454889e3b9ee5717d5f752ef91f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57d84febd46b4b09f88410b7ee23aafbdea22dc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TransflowerStoreWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TransflowerStoreWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

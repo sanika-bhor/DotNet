de376b6eb70e7ca85d6611229851795bac52a031
sanika-bhor <bhorsanika0239@gmail.com>
2025-01-06 21:37:46 +0530

    changes added

--- TransflowerStoreEcommerce/obj/Debug/net8.0/TransflowerStoreWeb.AssemblyInfo.cs
+++ TransflowerStoreEcommerce/obj/Debug/net8.0/TransflowerStoreWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TransflowerStoreWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff4c847870e5192cb16ae780c23dea00b3940c1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3be6d7a7db2fd85a86b58a2acc397385978cec52")]
 [assembly: System.Reflection.AssemblyProductAttribute("TransflowerStoreWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TransflowerStoreWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

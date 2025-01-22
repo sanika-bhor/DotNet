56441716f917d0b22870a9dd6bf40457280f8d20
sanika-bhor <bhorsanika0239@gmail.com>
2025-01-22 23:43:27 +0530

    server side statemanagement using sessions

--- SessionManagement/Program.cs
+++ SessionManagement/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//-----------------------------------------------------------------------------------------
+// register distributed cache memory for session management
+//DistributedMemory Cache
+//--it ia a separete cache memory which is reserved for session data.
+builder.Services.AddDistributedMemoryCache();
+//-----------------------------------------------------------------------------------------
+// add sestion 
+builder.Services.AddSession(options=>{
+    options.IdleTimeout=TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly=true;
+    options.Cookie.IsEssential=true;
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -15,7 +28,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+//setup session middleware
+app.UseSession();
 app.UseRouting();
 
 app.UseAuthorization();

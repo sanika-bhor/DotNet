20789c50c183b4d0b95d311fc3275770161e16a4
sanika-bhor <bhorsanika0239@gmail.com>
2025-01-22 21:45:06 +0530

    Configuration setting

--- ConfigurtionSetting/Controllers/HomeController.cs
+++ ConfigurtionSetting/Controllers/HomeController.cs
@@ -7,14 +7,29 @@ namespace ConfigurtionSetting.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    private readonly IConfiguration _configuration;
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration=configuration;
     }
 
     public IActionResult Index()
     {
+        return View();
+    }
+    public IActionResult Connect()
+    {
+        string connectionString=String.Empty;
+        var connection1=_configuration.GetConnectionString("ConnectionStringEcommerce");
+         var connection2=_configuration.GetConnectionString("ConnectionStringStudent");
+        
+         List<string> allconnection=new List<string>();
+         allconnection.Add(connection1);
+         allconnection.Add(connection2);
+        // connectionString=(string) connection1;
+        ViewData["connstring"]=allconnection;
+        // ViewData["connstring"]=connectionString;
         return View();
     }
 

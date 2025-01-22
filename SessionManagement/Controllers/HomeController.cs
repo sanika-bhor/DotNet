e4e755bfb63b6acd339732ce861219566f25a383
sanika-bhor <bhorsanika0239@gmail.com>
2025-01-23 00:07:35 +0530

    session management

--- SessionManagement/Controllers/HomeController.cs
+++ SessionManagement/Controllers/HomeController.cs
@@ -1,9 +1,27 @@
 using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc;
 using SessionManagement.Models;
 
 namespace SessionManagement.Controllers;
+[Serializable]
+public class Cart
+{
+   public List<string>items=new List<string>();
 
+    public Cart()
+    {
+        items.Add("mobile");
+        items.Add("Laptop");
+        items.Add("T.V");
+        items.Add("Frige");
+    }
+    public List<String> getAll()
+    {
+        return items;
+    }
+}
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
@@ -15,11 +33,24 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        string sessionKeyName="product";
+        HttpContext.Session.SetString(sessionKeyName,"Microwave");
+        HttpContext.Session.SetInt32("Age",19);
+
+        Cart cartItem = new Cart();
+        List<string> items=cartItem.getAll();
+        var str=JsonSerializer.Serialize(items);
+        HttpContext.Session.SetString("cart", str);
+
         return View();
     }
 
     public IActionResult Privacy()
     {
+        ViewBag.data=HttpContext.Session.GetString("product");
+        var dataItems=HttpContext.Session.GetString("cart");
+       var AllItems= JsonSerializer.Deserialize<Cart>(dataItems);
+        ViewData["cartItems"]=AllItems;
         return View();
     }
 

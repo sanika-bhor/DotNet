e125f9df398a698e264cd12c5e55740059ffdd28
sanika-bhor <bhorsanika0239@gmail.com>
2025-01-22 23:27:08 +0530

    Query string implementation

--- QueryStringDemo/Controllers/HomeController.cs
+++ QueryStringDemo/Controllers/HomeController.cs
@@ -18,6 +18,28 @@ public class HomeController : Controller
         return View();
     }
 
+    // use URL as:http://localhost:5158/home/querytest?Name=sanika&City=pune&state=maharastra
+    public IActionResult QueryTest()
+    {
+        string name=HttpContext.Request.Query["name"];
+        string city=HttpContext.Request.Query["city"];
+        string state=HttpContext.Request.Query["state"];
+        return Content("this is the querystring test invoked.......\n Name: "+name+"\n City: "+city+"\n State: "+state);
+    }
+
+    public IActionResult Student()
+    {
+        List<string> student=new List<string>();
+        student.Add("sanika");
+        student.Add("sumit");
+        student.Add("Ajinkya");
+        student.Add("Mansi");
+
+        var allstudent=student.ToArray();
+
+        return new JsonResult(allstudent);
+    }
+
     public IActionResult Privacy()
     {
         return View();

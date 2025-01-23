5b0f5ea6dc7548f550d5f05257ef52f57cff18c4
sanika-bhor <bhorsanika0239@gmail.com>
2025-01-23 22:00:39 +0530

    adding orm packages

--- ORMEntityFrameWork/Program.cs
+++ ORMEntityFrameWork/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+
+
+//dotnet add package Microsoft.EntityFrameworkCore
+//dotnet add package Microsoft.EntityFrameworkCore.Design
+//dotnet add package Microsoft.EntityFrameworkCore --version 8.0.20
--- ORMEntityFrameWork/obj/Debug/net8.0/ORMEntityFrameWork.AssemblyInfo.cs
+++ ORMEntityFrameWork/obj/Debug/net8.0/ORMEntityFrameWork.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ORMEntityFrameWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f47bc76b07ef3c1c18404c8d284072b8cae68b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2138b95c94a1868cb2a121690c4203a96b17aad4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ORMEntityFrameWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ORMEntityFrameWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

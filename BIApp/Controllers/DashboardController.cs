78b82c62b86b4976f504b39106d4616339347e6b
sanika-bhor <bhorsanika0239@gmail.com>
2025-01-31 23:04:26 +0530

    pie Chart

--- BIApp/Controllers/DashboardController.cs
+++ BIApp/Controllers/DashboardController.cs
@@ -32,6 +32,10 @@ public class DashboardController : Controller
     {
         return View();
     }
+    public IActionResult Index()
+    {
+        return View();
+    }
     
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
--- BIApp/obj/Debug/net8.0/BIApp.AssemblyInfo.cs
+++ BIApp/obj/Debug/net8.0/BIApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BIApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48197da43f9272ebead83aeeb534804f84f319e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+635f7f1f814b39a292c5ce619d9bcd85324d6c2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BIApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BIApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

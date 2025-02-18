8ea86b5eb88220c0d70b03ba113e314a2d5245bf
sanika-bhor <bhorsanika0239@gmail.com>
2025-02-18 12:53:53 +0530

    JWt Autherntication

---
+++ JWTAuthentication/Entities/User.cs
@@ -0,0 +1,14 @@
+using System.Text.Json.Serialization;
+
+namespace JWTAuthentication.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        [JsonIgnore]
+        public string Password { get; set; }
+    }
+}

769d8917fb538620ab27ac08fe1702ade751aecd
sanika-bhor <bhorsanika0239@gmail.com>
2026-05-07 20:02:42 +0530

    encryption

---
+++ Encryption/PasswordEncryption/Helpers/EncryptionHelper.cs
@@ -0,0 +1,65 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace backend.Helpers
+{
+    public class EncryptionHelper
+    {
+        public static string EncryptString(string plainText, string key)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter sw = new StreamWriter(cs))
+                        {
+                            sw.Write(plainText);
+                        }
+
+                        array = ms.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(array);
+        }
+
+        public static string DecryptString(string cipherText, string key)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream ms = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader sr = new StreamReader(cs))
+                        {
+                            return sr.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Encryption/PasswordEncryption/Program.cs
@@ -0,0 +1,18 @@
+﻿using backend.Helpers;
+class Program
+{
+    static void Main()
+    {
+        string plainText = "Hello World";
+
+        // 32-byte key for AES-256
+        string key = "12345678901234567890123456789012";
+
+        string encrypted = EncryptionHelper.EncryptString(plainText, key);
+        Console.WriteLine("Encrypted: " + encrypted);
+
+        string decrypted = EncryptionHelper.DecryptString(encrypted, key);
+        Console.WriteLine("Decrypted: " + decrypted);
+    }
+
+}

305cc6edbf9ab2620732c14e501e2125469be0a0
sanika-bhor <bhorsanika0239@gmail.com>
2026-06-19 14:31:52 +0530

    accountListner

---
+++ ActionListner/Listeners/AccountsDepartment.cs
@@ -0,0 +1,15 @@
+namespace ActionListener.Listener
+{
+    public class AccountsDepartment : IActionListener
+    {
+        public void onUnderBalance(double balance)
+        {
+            Console.WriteLine("Department! Current Balance: " + balance);
+        }
+
+        public void onOverBalance(double balance)
+        {
+            Console.WriteLine("Department! Current Balance: " + balance);
+        }
+    }
+}
---
+++ ActionListner/services/EmailService.cs
@@ -0,0 +1,11 @@
+namespace ActionListener.Services
+{
+    public class EmailService: NotificationService
+    {
+        public void send(string msg)
+        {
+            Console.WriteLine("Email: " + msg);
+        }
+         
+    }
+}
